631e5443e69a745a03cae95f68ec0453d4aaf9a3
Olayinka Otuniyi <o.otuniyi@elsevier.com>
2020-11-07 21:42:27 +0000

    token

---
+++ precache-manifest.8954a20af2443be5c3930c754d0f34de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ef2c993a2cbb7034a003ac024efafb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "591cc0c4ff3e34959113",
+    "url": "./static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "6a6b6f9fb5550e2be043",
+    "url": "./static/css/main.80e67ff0.chunk.css"
+  },
+  {
+    "revision": "591cc0c4ff3e34959113",
+    "url": "./static/js/2.c92ccae7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.c92ccae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a6b6f9fb5550e2be043",
+    "url": "./static/js/main.1d5919dd.chunk.js"
+  },
+  {
+    "revision": "1cb616d19eba6ddd9fab",
+    "url": "./static/js/runtime-main.8950de08.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a81af5fae7ef58978457d52ec850f658.js"
+  "./precache-manifest.8954a20af2443be5c3930c754d0f34de.js"
 );
 
 self.addEventListener('message', (event) => {
